2716daf0ddd5b6bb1886d3022f8a78ecf8d69659
juliogarciag <julioggonz@gmail.com>
2015-01-01 21:55:23 -0500

    fixed payload for rails requests

--- lib/restspec/rails/network_adapter.rb
+++ lib/restspec/rails/network_adapter.rb
@@ -10,7 +10,7 @@ module Restspec
       def request(request_object)
         public_send request_object.method,
                     request_object.url,
-                    request_object.payload,
+                    request_object.raw_payload,
                     request_object.headers
         
         [response.code.to_i, response.headers, response.body]
