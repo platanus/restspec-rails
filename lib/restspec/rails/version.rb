f934d068ce433ac51a65b91fd6ff5dd424ffe704
juliogarciag <julioggonz@gmail.com>
2015-01-01 21:59:12 -0500

    Added a patch for restspec-rails changes

--- lib/restspec/rails/version.rb
+++ lib/restspec/rails/version.rb
@@ -1,6 +1,7 @@
 module Restspec
   module Rails
     RESTSPEC_VERSION = '0.2.5'
-    VERSION = RESTSPEC_VERSION
+    RESTSPEC_RAILS_PATCH = '1'
+    VERSION = [RESTSPEC_VERSION, RESTSPEC_RAILS_PATCH].join('.')
   end
 end
